80e2ef42bef1441c15fa797eb598781dcaec33ad
Jinsoo <aaa119o1125@gmail.com>
2022-07-07 20:35:34 +0900

    일기 앱 만들기

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 김진수 on 2022/05/12.
+//
+
+import Foundation
+
+struct Diary {
+    var uuidString: String
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+    
+}
